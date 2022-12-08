0d71acbacf7586483f313ae7505d67fcfdda7ab5
mwangiwambui <wambui54mwangi@gmail.com>
2022-12-08 23:47:49 +0300

    Fixed the database connection issue

--- book_dir_api/index.js
+++ book_dir_api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const api = require('./routes/api');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3042;
 
 app.use(bodyParser.json());
 app.use('/', api);
